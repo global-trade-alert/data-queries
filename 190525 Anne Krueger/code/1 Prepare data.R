597299a3f9c28d478befc584f37add1cdc891d68
Johannes <fritz.johannes@gmail.com>
2019-05-28 10:33:14 +0200

    some tidying, nothing of substance

--- 190525 Anne Krueger/code/1 Prepare data.R
+++ 190525 Anne Krueger/code/1 Prepare data.R
@@ -6,8 +6,7 @@ library("lubridate")
 
 rm(list=ls())
 
-setwd("/Users/patrickbuess/Dropbox/Collaborations/GTA cloud")
-# setwd("C:/Users/Piotr Lukaszuk/Dropbox/GTA cloud")
+gta_setwd()
 
 output.path <- "4 data queries/190525 Anne Krueger/output/"
 
@@ -31,19 +30,19 @@ gta_data_slicer(gta.evaluation = c("Red","Amber"),
                 keep.implementation.na = F,
                 keep.affected = T,
                 keep.others = F,
-                in.force.today = T)
+                in.force.today = "Yes")
 
 master <- unique(master.sliced[,c("intervention.type", "intervention.id", "state.act.id", "title", "date.announced", "date.implemented", "date.removed", "gta.evaluation", "implementing.jurisdiction", "affected.sector", "affected.product", "affected.jurisdiction", "mast.id", "mast.chapter")])
 
 # ADD URL
-master$gta.url <- paste0("https://www.globaltradealert.org/state-act/", master$state.act.id)
+master$gta.url <- paste0("https://www.globaltradealert.org/intervention/", master$intervention.id)
 
 # REMOVE UNNECESSARY COLUMNS
 master.xlsx <- master
 master.xlsx[,c("intervention.id", "state.act.id")] <- NULL
 
 # SAVE EXCEL OF INTERVENTIONS
-write.xlsx(master.xlsx, file=paste0(output.path,"Trade remedy actions by USA against China, in force today.xlsx"), sheetName = "Interventions", row.names=F)
+xlsx::write.xlsx(master.xlsx, file=paste0(output.path,"Trade remedy actions by USA against China, in force today.xlsx"), sheetName = "Interventions", row.names=F)
 
 # GET INTERVENTION DESCRIPTIONS
 intervention.ids <- unique(master$intervention.id)
@@ -68,7 +67,7 @@ for(i in 1:nrow(interventions)) {
 source("0 report production/GTA 23/help files/GTA 23 cutoff and definitions.R")
 
 ## what are you doing here? Those seem to be the intervention IDs from the source.
-trump.steel.alu=c(27158, 27214)
+trump.steel.alu=c(27158)
 
 # GET LIST OF INTERVENTIONS
 gta_data_slicer(gta.evaluation = c("Red","Amber"),
@@ -114,43 +113,50 @@ covered.now=trade.coverage.estimates
 # how much of Chinese steel was 
 # already subject AD and CVD tariffs before the steel tariffs 
 # went generally into effect?
-covered.now[1,4]
-covered.already[1,4] # <-- that's the answer
+covered.now[1,ncol(covered.now)]
+covered.already[1,ncol(covered.already)] # <-- that's the answer
 
+coverages <- rbind(covered.now, covered.already)
+coverages$Type[1] <- "After Trump administration tariffs"
+coverages$Type[2] <- "Before Trump administration tariffs"
+coverages$`Importing country` <- "United States of America"
+coverages$`Exporting country` <- "China"
+coverages$`Number of interventions affecting exported product` <- NULL
 
+xlsx::write.xlsx(coverages, file=paste0(output.path,"Trade coverage estimates for Chinese steel exports to USA, before and after Trump steel & aluminum tariffs.xlsx"), sheetName = "Coverages", row.names=F)
 
-# WHEN WAS THE FIRST OF THESE INTERVENTIONS IMPLEMENTED?
-start.t.tariffs <- min(as.Date(interventions$date.implemented))
-
-# STEEL HS CODES, TAKEN FROM "GTA22/Annex/2 Steel trade exposure_new.R"
-steel.hs4 <- c(7206:7215, 7217:7229, 7304, 7306)
-steel.hs6 <- c(721610, 721621, 721622, 721631:721633, 721640, 721650, 730110, 730210, 730240, 730721:730723, 730729, 730791:730793, 730799)
-
-# CALCULATE COVERAGES FOR INTERVENTIONS BEFORE THE FIRST TRUMP TARIFF
-gta_trade_coverage(gta.evaluation=c("Red","Amber"),
-                   mast.chapters = c("D"),
-                   group.mast = F,
-                   keep.mast = T,
-                   hs.codes = steel.hs6,
-                   keep.hs = T,
-                   importers = 840,
-                   keep.importers = T,
-                   exporters = 156,
-                   keep.exporters = T,
-                   implementer.role = "importer",
-                   implementation.period = c("2008-01-01", paste(start.t.tariffs-1)),
-                   coverage.period = c(2009, 2019)
-)
-
-trade.coverage.estimates$`Number of interventions affecting exported product` <- NULL
-trade.coverage.estimates <- trade.coverage.estimates[2,]
-
-# PRETTIFY XLSX
-trade.coverage.estimates$`Importing country` <- "USA"
-trade.coverage.estimates$`Exporting country` <- "China"
-
-# WRITE XLSX
-write.xlsx(trade.coverage.estimates, file=paste0(output.path,"Trade coverages for Chinese steel exports to USA.xlsx"), sheetName = "Coverages", row.names=F)
+# # WHEN WAS THE FIRST OF THESE INTERVENTIONS IMPLEMENTED?
+# start.t.tariffs <- min(as.Date(interventions$date.implemented))
+# 
+# # STEEL HS CODES, TAKEN FROM "GTA22/Annex/2 Steel trade exposure_new.R"
+# steel.hs4 <- c(7206:7215, 7217:7229, 7304, 7306)
+# steel.hs6 <- c(721610, 721621, 721622, 721631:721633, 721640, 721650, 730110, 730210, 730240, 730721:730723, 730729, 730791:730793, 730799)
+# 
+# # CALCULATE COVERAGES FOR INTERVENTIONS BEFORE THE FIRST TRUMP TARIFF
+# gta_trade_coverage(gta.evaluation=c("Red","Amber"),
+#                    mast.chapters = c("D"),
+#                    group.mast = F,
+#                    keep.mast = T,
+#                    hs.codes = steel.hs6,
+#                    keep.hs = T,
+#                    importers = 840,
+#                    keep.importers = T,
+#                    exporters = 156,
+#                    keep.exporters = T,
+#                    implementer.role = "importer",
+#                    implementation.period = c("2008-01-01", paste(start.t.tariffs-1)),
+#                    coverage.period = c(2009, 2019)
+# )
+# 
+# trade.coverage.estimates$`Number of interventions affecting exported product` <- NULL
+# trade.coverage.estimates <- trade.coverage.estimates[2,]
+# 
+# # PRETTIFY XLSX
+# trade.coverage.estimates$`Importing country` <- "USA"
+# trade.coverage.estimates$`Exporting country` <- "China"
+# 
+# # WRITE XLSX
+# write.xlsx(trade.coverage.estimates, file=paste0(output.path,"Trade coverages for Chinese steel exports to USA.xlsx"), sheetName = "Coverages", row.names=F)
 
 
 
@@ -178,5 +184,5 @@ trade.coverage.estimates$`Importing country` <- "USA"
 trade.coverage.estimates$`Exporting country` <- "World"
 
 # WRITE XLSX
-write.xlsx(trade.coverage.estimates, file=paste0(output.path,"Trade coverages for US imports.xlsx"), sheetName = "Coverages", row.names=F)
+xlsx::write.xlsx(trade.coverage.estimates, file=paste0(output.path,"Trade coverage estimates for US imports affected by MAST D.xlsx"), sheetName = "Coverages", row.names=F)
 
