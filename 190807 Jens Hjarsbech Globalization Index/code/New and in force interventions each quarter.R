5c46529cf056ef7ceef65c40d794a222d684a2d1
Johannes <fritz.johannes@gmail.com>
2019-08-08 10:44:30 +0200

    "harmful" in GTA speak means "amber or red"

--- 190807 Jens Hjarsbech Globalization Index/code/New and in force interventions each quarter.R
+++ 190807 Jens Hjarsbech Globalization Index/code/New and in force interventions each quarter.R
@@ -18,10 +18,12 @@ data.path='4 data queries/190807 Jens Hjarsbech Globalization Index/data/'
 
 gta_data_slicer()
 base.data=subset(master.sliced, a.un %in% subset(country.names,is.eu==T)$un_code)
-base.data=unique(subset(base.data,!is.na(date.implemented) & date.implemented<=Sys.Date() & gta.evaluation %in% c('Red','Green'), select=c('date.implemented','date.removed','intervention.id','gta.evaluation','affected.jurisdiction')))
+base.data$stance="harmful"
+base.data$stance[base.data$gta.evaluation=="Green"]="liberalising"
+base.data=unique(subset(base.data,!is.na(date.implemented) & date.implemented<=Sys.Date(), select=c('date.implemented','date.removed','intervention.id','stance','affected.jurisdiction')))
 
-eu.base=unique(subset(base.data,select=c('date.implemented','date.removed','intervention.id','gta.evaluation')))
-dk.base=unique(subset(base.data,affected.jurisdiction=='Denmark',select=c('date.implemented','date.removed','intervention.id','gta.evaluation')))
+eu.base=unique(subset(base.data,select=c('date.implemented','date.removed','intervention.id','stance')))
+dk.base=unique(subset(base.data,affected.jurisdiction=='Denmark',select=c('date.implemented','date.removed','intervention.id','stance')))
 
 # eu ----------------------------------------------------------------------
 for(base in 1:length(list(eu.base,dk.base))){
@@ -31,10 +33,10 @@ for(base in 1:length(list(eu.base,dk.base))){
   new.ids=data
   in.force.ids=data
   
-  new.ids=unique(subset(new.ids,select=c('date.implemented','intervention.id','gta.evaluation')))
+  new.ids=unique(subset(new.ids,select=c('date.implemented','intervention.id','stance')))
   new.ids$qtr.impl=as.yearqtr(as.Date(new.ids$date.implemented,"%Y-%m-%d"))
-  new.ids=aggregate(intervention.id~qtr.impl+gta.evaluation,new.ids,function(x) length(unique(x)))
-  new.ids=spread(new.ids,gta.evaluation,intervention.id)
+  new.ids=aggregate(intervention.id~qtr.impl+stance,new.ids,function(x) length(unique(x)))
+  new.ids=spread(new.ids,stance,intervention.id)
   new.ids=merge(data.frame(qtr.impl=as.yearqtr(paste0(rep(seq(2008,2019,1),each=4),' Q',1:4))),new.ids,by='qtr.impl',all.x=T)
   new.ids=new.ids[-nrow(new.ids),]
   new.ids[is.na(new.ids)]=0
@@ -43,7 +45,7 @@ for(base in 1:length(list(eu.base,dk.base))){
   
   in.force.ids$qtr.impl=as.yearqtr(as.Date(in.force.ids$date.implemented,"%Y-%m-%d"))
   in.force.ids$qtr.rem=as.yearqtr(as.Date(in.force.ids$date.removed,"%Y-%m-%d"))
-  in.force.ids=subset(in.force.ids,select=c('qtr.impl','qtr.rem','gta.evaluation'))
+  in.force.ids=subset(in.force.ids,select=c('qtr.impl','qtr.rem','stance'))
   
   #if implemented in a quarter, then it is considered in force in this quarter (<=)
   #if removed during a quarter, then it is not considered in force this quarter (<)
@@ -54,11 +56,11 @@ for(base in 1:length(list(eu.base,dk.base))){
     
   }
   
-  red.in.force.ids=subset(in.force.ids,gta.evaluation=='Red',select=!(names(in.force.ids) %in% c('qtr.impl','qtr.rem','gta.evaluation')))
+  red.in.force.ids=subset(in.force.ids,stance=='harmful',select=!(names(in.force.ids) %in% c('qtr.impl','qtr.rem','stance')))
   red.in.force.ids=data.frame(quarter=names(red.in.force.ids),
                           in.force.interventions=colSums(red.in.force.ids))
   
-  green.in.force.ids=subset(in.force.ids,gta.evaluation=='Green',select=!(names(in.force.ids) %in% c('qtr.impl','qtr.rem','gta.evaluation')))
+  green.in.force.ids=subset(in.force.ids,stance=='liberalising',select=!(names(in.force.ids) %in% c('qtr.impl','qtr.rem','stance')))
   green.in.force.ids=data.frame(quarter=names(green.in.force.ids),
                               in.force.interventions=colSums(green.in.force.ids))
 
